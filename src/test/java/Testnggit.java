b05239bece8d252269106cca609f93d3d916c03a
Sagar <smitha.raval@gmail.com>
2019-09-14 17:04:48 +0530

    MyFirstCode-creation of annotation

--- src/test/java/Testnggit.java
+++ src/test/java/Testnggit.java
@@ -1,4 +1,39 @@
+import org.testng.annotations.Test;
 
 public class Testnggit {
 
+	@Test 
+	public void openBrowser()
+	
+	{
+		System.out.println("Open browser");
+	}
+
+	@Test 
+	public void LaunchURL()
+	
+	{
+		System.out.println("Launch url");
+	}
+	
+	@Test 
+	public void SearchProduct()
+	
+	{
+		System.out.println("search product");
+	}
+	@Test 
+	public void VerifyProduct()
+	
+	{
+		System.out.println("verify product");
+	}
+	
+	@Test 
+	public void Closebrowser()
+	
+	{
+		System.out.println("close browser");
+	}
 }
+
